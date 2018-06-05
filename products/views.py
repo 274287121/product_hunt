6080047f20c89f68d4310ba27b35ad1c94bfd407
Unknown <274287121@qq.com>
2018-06-05 13:47:27 +0800

    初步完成了APP发布页面
    完成了前端部分

--- products/views.py
+++ products/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def product_list(request):
     return render(request, 'product_list.html')
+
+
+def publish(request):
+    if request.method == 'GET':
+        return render(request, 'publish.html')
+    elif request.method == 'POST':
+        app_name = request.POST['APP名称']
+        intro = request.POST['介绍']
+        url = request.POST['APP链接']
+        icon = request.FILES['APP缩略图']
+        image = request.FILES['APP大图']
+        return render(request, 'publish.html')
